586894d24e653c2e7455f26fecd2e4582df568d7
RayWong <feelingswl@gmail.com>
2020-03-06 16:32:29 +0800

    []byteToBinaryString

--- encode/binary.go
+++ encode/binary.go
@@ -1,1 +1,30 @@
 package encode
+
+// BytesToBinaryString get the string in binary format of a []byte or []int8.
+func BytesToBinaryString(bs []byte) string {
+	l := len(bs)
+	bl := l*8
+	buf := make([]byte, 0, bl)
+	for _, b := range bs {
+		buf = appendBinaryString(buf, b)
+	}
+	return string(buf)
+}
+
+// append bytes of string in binary format.
+func appendBinaryString(bs []byte, b byte) []byte {
+	var a byte
+	for i := 0; i < 8; i++ {
+		a = b
+		b <<= 1
+		b >>= 1
+		switch a {
+		case b:
+			bs = append(bs, byte('0'))
+		default:
+			bs = append(bs, byte('1'))
+		}
+		b <<= 1
+	}
+	return bs
+}
