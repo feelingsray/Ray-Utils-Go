1c6c00980c59c239cb24b96409f187d72aa38dba
RayWong <feelingswl@gmail.com>
2021-01-05 09:25:58 +0800

    crc32 hash

---
+++ encode/crc.go
@@ -0,0 +1,13 @@
+package encode
+
+import "hash/crc32"
+
+/*
+ * 通过CRC32方式hash字符串为数字并取模
+ */
+func Crc32HashKey(key string,mod uint32) (uint32,uint32) {
+    scratch := []byte(key)
+    hres := crc32.ChecksumIEEE(scratch)
+    mres := hres % mod
+    return hres,mres
+}
