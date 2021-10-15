747f01f6708205dbb5cf23167a690f7373b314e4
RayWong <feelingswl@gmail.com>
2021-10-16 02:13:08 +0800

    222

--- example/otp/totp.go
+++ example/otp/totp.go
@@ -7,12 +7,8 @@ import (
 )
 
 func main() {
-
-    d,_ := time.ParseDuration("-1h")
-    e,_ := time.ParseDuration("-2h")
-    _ = e
-    s := rotp.RTOTPCodeWithTime("rayray00",time.Now().Add(d))
+    s := rotp.RTOTPCodeWithTime("RAYRAY22",time.Now())
     fmt.Println(s)
-    fmt.Println(rotp.RTOTPVerifyWithTime(s,time.Now().Add(d)))
+    fmt.Println(rotp.RTOTPVerifyWithTime(s,time.Now()))
 
 }
