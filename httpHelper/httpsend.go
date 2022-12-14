a10bf45a0e6971aa4bf911cfd34e6aa67d4b1b08
张国庆 <iwanlebron@foxmail.com>
2022-12-14 21:10:06 +0800

    del:unuse code

--- httpHelper/httpsend.go
+++ httpHelper/httpsend.go
@@ -1,64 +1,64 @@
 package httpHelper
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/feelingsray/Ray-Utils-Go/tools/common"
+  "bytes"
+  "io"
+  "io/ioutil"
+  "net/http"
+  
+  "github.com/feelingsray/Ray-Utils-Go/encode"
 )
 
 func HttpPostWithAuth(url string, body any, username string, password string) (int, string, error) {
-
-	bodyJson, err := common.DumpJson(body)
-	if err != nil {
-		return 500, "", err
-	}
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(username, password)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return 500, "", err
-	}
-	defer resp.Body.Close()
-	respBody, _ := ioutil.ReadAll(resp.Body)
-	return resp.StatusCode, string(respBody), nil
-
+  
+  bodyJson, err := encode.DumpJson(body)
+  if err != nil {
+    return 500, "", err
+  }
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
+  req.Header.Set("Content-Type", "application/json")
+  req.SetBasicAuth(username, password)
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return 500, "", err
+  }
+  defer resp.Body.Close()
+  respBody, _ := ioutil.ReadAll(resp.Body)
+  return resp.StatusCode, string(respBody), nil
+  
 }
 
 func HttpPost(url string, body any) (int, string, error) {
-
-	bodyJson, err := common.DumpJson(body)
-	if err != nil {
-		return 500, "", err
-	}
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
-	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return 500, "", err
-	}
-	defer resp.Body.Close()
-	respBody, _ := ioutil.ReadAll(resp.Body)
-	return resp.StatusCode, string(respBody), nil
-
+  
+  bodyJson, err := encode.DumpJson(body)
+  if err != nil {
+    return 500, "", err
+  }
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
+  req.Header.Set("Content-Type", "application/json")
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  
+  if err != nil {
+    return 500, "", err
+  }
+  defer resp.Body.Close()
+  respBody, _ := ioutil.ReadAll(resp.Body)
+  return resp.StatusCode, string(respBody), nil
+  
 }
 
 func HttpGet(url string) (int, string, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return 500, "", err
-	}
-	defer resp.Body.Close()
-	respBody, _ := io.ReadAll(resp.Body)
-	return resp.StatusCode, string(respBody), nil
-
+  req, err := http.NewRequest("GET", url, nil)
+  req.Header.Set("Content-Type", "application/json")
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return 500, "", err
+  }
+  defer resp.Body.Close()
+  respBody, _ := io.ReadAll(resp.Body)
+  return resp.StatusCode, string(respBody), nil
+  
 }
