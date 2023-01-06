60d8c7d5be2d138ac6b6b59434ccea3d585f28ae
张国庆 <iwanlebron@foxmail.com>
2023-01-06 10:06:00 +0800

    feat:readme&format

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Ray-Utils-Go
+## Ray-Utils-Go
 
-精英数智物联网团队工具包
+### 1.主要包含功能
+
++ 协程管理器
++ 多种编解码
++ http中间件
++ 客户端连接
++ 通用工具包
++ 日志管理器
+
+### 2.目录说明
+
+    Ray-Utils-Go
+    ├── appManager          协程管理
+    ├── autoUpdate          自动更新
+    ├── cached              内存缓存
+    ├── crc16               crc加密
+    ├── encode              编解码器
+    ├── httpHelper          http辅助
+    ├── logger              日志管理
+    ├── middleware          http中间件
+    ├── minio               对象存储
+    ├── mongo               Mongo连接
+    ├── netHepler           IP帮助
+    ├── newMongo            Mongo连接
+    ├── rayProxy            网络代理
+    ├── rotp                动态密码
+    ├── secret              加密工具
+    ├── tarHelper           压缩解压
+    ├── tools               工具软件
+
+### 3.go版本 >=1.18
+
+    > go version
+
+<div style="text-align: center;">精英数智物联网团队</div>
--- rayProxy/services/args.go
+++ rayProxy/services/args.go
@@ -4,77 +4,77 @@ package services
 // t := tcp.Flag("tcp-timeout", "tcp timeout milliseconds when connect to real server or parent proxy").Default("2000").Int()
 
 const (
-    TYPE_TCP     = "tcp"
-    TYPE_UDP     = "udp"
-    TYPE_HTTP    = "http"
-    TYPE_TLS     = "tls"
-    CONN_CONTROL = uint8(1)
-    CONN_SERVER  = uint8(2)
-    CONN_CLIENT  = uint8(3)
+	TYPE_TCP     = "tcp"
+	TYPE_UDP     = "udp"
+	TYPE_HTTP    = "http"
+	TYPE_TLS     = "tls"
+	CONN_CONTROL = uint8(1)
+	CONN_SERVER  = uint8(2)
+	CONN_CLIENT  = uint8(3)
 )
 
 type Args struct {
-    Local     *string
-    Parent    *string
-    CertBytes []byte
-    KeyBytes  []byte
+	Local     *string
+	Parent    *string
+	CertBytes []byte
+	KeyBytes  []byte
 }
 
 type TunnelServerArgs struct {
-    Args
-    IsUDP   *bool
-    Key     *string
-    Timeout *int
+	Args
+	IsUDP   *bool
+	Key     *string
+	Timeout *int
 }
 
 type TunnelClientArgs struct {
-    Args
-    IsUDP   *bool
-    Key     *string
-    Timeout *int
+	Args
+	IsUDP   *bool
+	Key     *string
+	Timeout *int
 }
 
 type TunnelBridgeArgs struct {
-    Args
-    Timeout *int
+	Args
+	Timeout *int
 }
 
 type TCPArgs struct {
-    Args
-    ParentType          *string
-    IsTLS               *bool
-    Timeout             *int
-    PoolSize            *int
-    CheckParentInterval *int
+	Args
+	ParentType          *string
+	IsTLS               *bool
+	Timeout             *int
+	PoolSize            *int
+	CheckParentInterval *int
 }
 
 type HTTPArgs struct {
-    Args
-    Always              *bool
-    HTTPTimeout         *int
-    Interval            *int
-    Blocked             *string
-    Direct              *string
-    AuthFile            *string
-    Auth                *[]string
-    ParentType          *string
-    LocalType           *string
-    Timeout             *int
-    PoolSize            *int
-    CheckParentInterval *int
+	Args
+	Always              *bool
+	HTTPTimeout         *int
+	Interval            *int
+	Blocked             *string
+	Direct              *string
+	AuthFile            *string
+	Auth                *[]string
+	ParentType          *string
+	LocalType           *string
+	Timeout             *int
+	PoolSize            *int
+	CheckParentInterval *int
 }
 
 type UDPArgs struct {
-    Args
-    ParentType          *string
-    Timeout             *int
-    PoolSize            *int
-    CheckParentInterval *int
+	Args
+	ParentType          *string
+	Timeout             *int
+	PoolSize            *int
+	CheckParentInterval *int
 }
 
 func (a *TCPArgs) Protocol() string {
-    if *a.IsTLS {
-        return "tls"
-    }
-    return "tcp"
+	if *a.IsTLS {
+		return "tls"
+	}
+	return "tcp"
 }
