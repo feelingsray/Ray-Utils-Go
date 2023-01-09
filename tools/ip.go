fd22ac14aff4c2ff7ab2978d70dc887dd695f88a
张国庆 <iwanlebron@foxmail.com>
2023-01-09 11:06:49 +0800

    feat:manager

---
+++ tools/ip.go
@@ -0,0 +1,17 @@
+package tools
+
+import "net"
+
+func GetIPAddressByName(name string) string {
+	addr, _ := net.InterfaceByName(name)
+	addrs, _ := addr.Addrs()
+	data := ""
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				data = ipnet.IP.String()
+			}
+		}
+	}
+	return data
+}
